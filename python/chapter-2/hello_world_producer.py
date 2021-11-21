9582d9bc8eff6528ce8bd5c7051ec587f4579c2e
lmw0509 <32791558+lmw0509@users.noreply.github.com>
2021-11-21 22:21:03 +0800

    Update hello_world_producer.py

--- python/chapter-2/hello_world_producer.py
+++ python/chapter-2/hello_world_producer.py
@@ -11,24 +11,23 @@
 import pika, sys
 
 credentials = pika.PlainCredentials("guest", "guest")
-conn_params = pika.ConnectionParameters("localhost",
-                                        credentials = credentials)
-conn_broker = pika.BlockingConnection(conn_params) #/(hwp.1) Establish connection to broker
+conn_params = pika.ConnectionParameters("192.168.61.10",
+                                        credentials=credentials)
+conn_broker = pika.BlockingConnection(conn_params)  # /(hwp.1) Establish connection to broker
 
+channel = conn_broker.channel()  # /(hwp.2) Obtain channel
 
-channel = conn_broker.channel() #/(hwp.2) Obtain channel
-
-channel.exchange_declare(exchange="hello-exchange", #/(hwp.3) Declare the exchange
-                         type="direct",
+channel.exchange_declare(exchange="hello-exchange",  # /(hwp.3) Declare the exchange
+                         exchange_type="direct",
                          passive=False,
                          durable=True,
                          auto_delete=False)
 
 msg = sys.argv[1]
 msg_props = pika.BasicProperties()
-msg_props.content_type = "text/plain" #/(hwp.4) Create a plaintext message
+msg_props.content_type = "text/plain"  # /(hwp.4) Create a plaintext message
 
 channel.basic_publish(body=msg,
                       exchange="hello-exchange",
                       properties=msg_props,
-                      routing_key="hola") #/(hwp.5) Publish the message
+                      routing_key="hola")  # /(hwp.5) Publish the message
